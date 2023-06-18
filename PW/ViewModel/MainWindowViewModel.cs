352c3b6588ca625630f22e949924c5875a8e823a
Julia Maciaszczyk <236589@edu.p.lodz.pl>
2023-06-18 19:37:37 +0200

    vm

--- PW/ViewModel/MainWindowViewModel.cs
+++ PW/ViewModel/MainWindowViewModel.cs
@@ -7,51 +7,155 @@ namespace ViewModel
 
     public class MainWindowViewModel : BaseViewModel
     {
-        private readonly ModelAbstractApi ModelLayer;
-        private int _BallVal;
+ private readonly ModelAbstractApi ModelLayer;
+        private int _BallVal = 1;
+        private int width;
+        private int height;
+        private bool _isStopEnabled = false;
+        private bool isStartEnabled = false;
+        private bool _isAddEnabled = true;
+        private int size = 0;
+        private IList _balls;
         public ICommand AddCommand { get; set; }
-
-        public MainWindowViewModel()
-        {
-
-            ModelLayer = ModelAbstractApi.CreateApi(600, 480);
-            StopCommand = new RelayCommand(Stop);
-            AddCommand = new RelayCommand(CreateEllipses);
-
-        }
-
-
+        public ICommand RunCommand { get; set; }
         public ICommand StopCommand
         { get; set; }
 
-
-        public int BallVal
+        public MainWindowViewModel()
         {
-            get { return _BallVal; }
+            width = 600;
+            height = 480;
+            ModelLayer = ModelAbstractApi.CreateApi(600, 480);
+            StopCommand = new RelayCommand(Stop);
+            AddCommand = new RelayCommand(CreateEllipses);
+            RunCommand = new RelayCommand(Start);
+        }
+        public bool isStopEnabled
+        {
+            get { return _isStopEnabled; }
             set
             {
-                _BallVal = value;
+                _isStopEnabled = value;
                 RaisePropertyChanged();
             }
         }
 
-        public Canvas Canvas
+        public bool isRunEnabled
         {
-            get => ModelLayer.Canvas;
-
+            get { return isStartEnabled; }
+            set
+            {
+                isStartEnabled = value;
+                RaisePropertyChanged();
+            }
         }
 
-        private void CreateEllipses()
+        public bool isAddEnabled
         {
-            ModelLayer.CreateEllipses(BallVal);
+            get
+            {
+                return _isAddEnabled;
+            }
+            set
+            {
+                _isAddEnabled = value;
+
+                RaisePropertyChanged();
+            }
+        }
+
+        public int BallVal
+        {
+            get
+            {
+
+                return _BallVal;
+            }
+            set
+            {
+
+                _BallVal = value;
+                RaisePropertyChanged();
+
+
+            }
+
+        }
+        public int Width
+        {
+            get
+            {
+
+                return width;
+            }
+            set
+            {
+
+                width = value;
+                RaisePropertyChanged();
+            }
+
+        }
+        public int Height
+        {
+            get
+            {
+
+                return height;
+            }
+            set
+            {
+
+                height = value;
+                RaisePropertyChanged();
+            }
+
+        }
+        private void AddBalls()
+        {
+            size += BallVal;
+            if (size > 0)
+            {
+                isRunEnabled = true;
+            }
+            else
+            {
+                size = 0;
+                isRunEnabled = false;
+            }
+            Balls = ModelLayer.Start(BallVal);
+            BallVal = 1;
+
+
         }
         private void Stop()
         {
-
+            isStopEnabled = false;
+            isAddEnabled = true;
+            isRunEnabled = true;
             ModelLayer.Stop();
-
         }
+        private void Start()
+        {
+            isStopEnabled = true;
+            isRunEnabled = false;
+            isAddEnabled = false;
+            ModelLayer.StartMoving();
+        }
+        public IList Balls
+        {
+            get => _balls;
+            set
+            {
+                if (value.Equals(_balls))
+                {
+                    return;
+                }
 
+                _balls = value;
+                RaisePropertyChanged();
+            }
+        }
 
 
     }
