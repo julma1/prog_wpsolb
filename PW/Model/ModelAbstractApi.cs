d3bf7c8dca31081bbfca1b55593da071c975840d
Julia Maciaszczyk <236589@edu.p.lodz.pl>
2023-06-19 01:21:25 +0200

    mode

--- PW/Model/ModelAbstractApi.cs
+++ PW/Model/ModelAbstractApi.cs
@@ -9,6 +9,8 @@ namespace Model
         public abstract int height { get; }
         public abstract void StartMoving();
         public abstract IList Start(int ballVal);
+
+        public abstract IList Delete(int ballVal);
         public abstract void Stop();
 
         public static ModelAbstractApi CreateApi(int Weight, int Height)
@@ -16,36 +18,5 @@ namespace Model
             return new ModelApi(Weight, Height);
         }
     }
-    internal class ModelApi : ModelAbstractApi
-    {
-        public override int width { get; }
-        public override int height { get; }
-        private readonly LogicAbstractApi LogicLayer;
-
-        public ModelApi(int Width, int Height)
-        {
-
-            width = Width;
-            height = Height;
-            LogicLayer = LogicAbstractApi.CreateApi(width, height);
-
-
-        }
-
-        public override void StartMoving()
-        {
-            LogicLayer.Start();
-        }
-
-
-        public override void Stop()
-        {
-            LogicLayer.Stop();
-        }
-
-        public override IList Start(int ballVal) => LogicLayer.CreateBalls(ballVal);
-
-    }
-
 }
 
