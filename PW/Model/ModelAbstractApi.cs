9ea14501c57140ae3d430433aaf46fc8f935ff90
Julia Maciaszczyk <236589@edu.p.lodz.pl>
2023-06-18 19:27:48 +0200

    model

--- PW/Model/ModelAbstractApi.cs
+++ PW/Model/ModelAbstractApi.cs
@@ -1,11 +1,5 @@
 ﻿using Logic;
 using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Shapes;
-
-
 
 namespace Model
 {
@@ -13,13 +7,9 @@ namespace Model
     {
         public abstract int width { get; }
         public abstract int height { get; }
-        public abstract Canvas Canvas { get; set; }
-        public abstract List<Ellipse> ellipseCollection { get; }
-        public abstract void CreateEllipses(int ballVal);
-        public abstract void Move();
-
+        public abstract void StartMoving();
+        public abstract IList Start(int ballVal);
         public abstract void Stop();
-
 
         public static ModelAbstractApi CreateApi(int Weight, int Height)
         {
@@ -30,59 +20,32 @@ namespace Model
     {
         public override int width { get; }
         public override int height { get; }
+        private readonly LogicAbstractApi LogicLayer;
 
-        private LogicAbstractApi LogicLayer;
-        public override List<Ellipse> ellipseCollection { get; }
-        public override Canvas Canvas { get; set; }
         public ModelApi(int Width, int Height)
         {
 
             width = Width;
             height = Height;
             LogicLayer = LogicAbstractApi.CreateApi(width, height);
-            ellipseCollection = new List<Ellipse>();
-            Canvas = new Canvas();
-            Canvas.HorizontalAlignment = HorizontalAlignment.Center;
-            Canvas.VerticalAlignment = VerticalAlignment.Top;
-            Canvas.Width = width;
-            Canvas.Height = height;
-            Canvas.Background = new SolidColorBrush(Color.FromRgb(241, 237, 237));
-            LogicLayer.Update += (sender, args) => Move();
+
+
         }
-        public override void CreateEllipses(int ballVal)
+
+        public override void StartMoving()
         {
-            LogicLayer.CreateBallsList(ballVal);
-            
-            for (int i = LogicLayer.GetCount - ballVal; i < LogicLayer.GetCount; i++)
-            {
-                Ellipse ellipse = new Ellipse { Width = LogicLayer.GetSize(i), Height = LogicLayer.GetSize(i), Fill = Brushes.Black };
-                Canvas.SetLeft(ellipse, LogicLayer.GetX(i));
-                Canvas.SetTop(ellipse, LogicLayer.GetY(i));
-                ellipseCollection.Add(ellipse);
-                Canvas.Children.Add(ellipse);
-            }
             LogicLayer.Start();
-
         }
 
-        public override void Move()
-        {
-            for (int i = 0; i < LogicLayer.GetCount; i++)
-            {
-                Canvas.SetLeft(ellipseCollection[i], LogicLayer.GetX(i));
-                Canvas.SetTop(ellipseCollection[i], LogicLayer.GetY(i));
-            }
-            for (int i = LogicLayer.GetCount; i < ellipseCollection.Count; i++)
-            {
-                Canvas.Children.Remove(ellipseCollection[ellipseCollection.Count - 1]);
-                ellipseCollection.Remove(ellipseCollection[ellipseCollection.Count - 1]);
-            }
-        }
 
         public override void Stop()
         {
             LogicLayer.Stop();
         }
+
+        public override IList Start(int ballVal) => LogicLayer.CreateBalls(ballVal);
+
     }
 
 }
+
